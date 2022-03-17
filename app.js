e9a0c9ea37a21f5d6193cf52d80d66dc7bcb201d
Silva <raul.lima.da.silva@accenture.com>
2022-03-16 23:25:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemonPromises = () => Array(898).fill().map((_, index) =>
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types, sprites}) => {
+    const elementTypes = types.map(typeinfo => typeinfo.type.name)
+    
+    accumulator += `
+        <li class="card ${elementTypes[0]}">
+        <img class="card-image" alt="${name}" src="${sprites.other.home.front_default}" />
+            <h2 class="card-title">
+                ${id}. ${name}
+            </h2>
+            <p class="card-subtitle">
+                ${elementTypes.join(' | ')}
+            </p>
+        </li>
+    `
+    return accumulator
+}, '')
+
+const inserPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+
+   ul.innerHTML = pokemons
+} 
+
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(inserPokemonsIntoPage)
